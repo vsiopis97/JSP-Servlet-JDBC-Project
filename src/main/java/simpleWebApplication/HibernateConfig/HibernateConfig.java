abcf1e053aba0f2f7667de57344b1dc5c9882a29
vsiopis97 <vsiopis97@gmail.com>
2023-01-07 16:12:29 +0200

    Some MORE comments

--- src/main/java/simpleWebApplication/HibernateConfig/HibernateConfig.java
+++ src/main/java/simpleWebApplication/HibernateConfig/HibernateConfig.java
@@ -10,6 +10,7 @@ import org.hibernate.service.ServiceRegistry;
 
 import simpleWebApplication.User.*;
 
+//Hibernate configuration.xml commands class
 public class HibernateConfig{
 	private static SessionFactory sessionFactory;
 
